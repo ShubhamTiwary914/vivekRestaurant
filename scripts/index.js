5e55800113b23a94e65d6e933ba2ddcb0eab1e34
Shubam <shubhamtiwary914@gmail.com>
2023-04-01 15:28:28 +0530

    Home Page updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,5 @@
 
 
-let orderType_HTML = {
-    "delivery": `
-        <h4 class="card-title">Order food to your doorsteps!</h4>
-        <br /> 
-        <p class="card-text">
-            <label class='form-label'> <h5> Locate your delivery address: </h5>  </label> &nbsp;&nbsp;&nbsp;
-            <button class='btn'id='order-findLocation'> Find Location </button> <br /> <br />
-            <input id='order-location' placeholder='Your current location...'/> 
-        </p>
-        <button class="btn btn-success disabled" id='order-button'>Make Order</button>
-    `,
-    "dine": `
-        <h4 class="card-title">Check food to dine in and eat!</h4>
-        <p class="card-text">
-            <label class='form-label'>  </label>
-        </p>
-        <a href="#" class="btn btn-success menuPage-linker"> View Menu </a>
-    `
-}
-
-
-function ordertypeManager(orderType, setDefault = false){
-    if(setDefault){
-        $(`#order-type-delivery`).css({ 'background-color': 'white', 'color': '#a52f26' });
-        $('#order-manageSection .card-body').html(orderType_HTML['delivery']);
-        return;
-    }
-    if(orderType == 'delivery'){
-        $(`#order-type-delivery`).css({ 'background-color': 'white', 'color': '#a52f26' });
-        $(`#order-type-dine`).css({ 'background-color': 'black', 'color': 'white' });
-    }else{
-        $(`#order-type-dine`).css({ 'background-color': 'white', 'color': '#a52f26' });
-        $(`#order-type-delivery`).css({ 'background-color': 'black', 'color': 'white' });
-    }
-   $('#order-manageSection .card-body').html(orderType_HTML[orderType]);
-}
-
-
 function findUserCoordinates(){
     const success = (position) =>{
         let longt = position.coords.longitude;
@@ -66,12 +28,37 @@ function locate_userLocation(addressCoords){
         //address details
         let streetName = fetchedResult[resultsCount - 1]['street']
         let addressName = fetchedResult[resultsCount - 1]['name']
-        let postCode = fetchedResult[resultsCount - 1]['postcode']
         let district = fetchedResult[resultsCount - 1]['state_district']
         let state = fetchedResult[resultsCount - 1]['state']
-        $('#order-location').val(`${addressName}, ${streetName}(${postCode}), ${district}, ${state}`);
-        $('#order-button').removeClass('disabled')
+        let userLocationDetails = `${addressName}, ${streetName}, ${district}, ${state}`;
+        console.log(userLocationDetails)
     })
+}
+
+
+
+function handleSelectionOptions(selectedOption){
+    let selectionContainer_HTML = '';
+    if(selectedOption == 'delivery'){
+        selectionContainer_HTML = `  
+            <div class='row'>
+                <div class='col-7'>
+                    <input class='form-control' placeholder='Enter your delivery address' id='delivery-input'>
+                </div>
+                <div class='col-3 gray-text' id='locate-btn'> 
+                    <img src='./assets/icons/svgs/locatorPin.svg' id='locator-icon'>
+                    <span id='locate-text'> Locate me  </span>
+                </div>
+                <div class='col-2'> <button class='btn menu-btn' id='find-food-btn'> Find Food </button> </div>
+            </div>
+        `;
+    }else if(selectedOption == 'dine'){
+        selectionContainer_HTML = `
+            <em style='color: gray'> Select from our varieties of food available here </em> &nbsp; &nbsp; &nbsp;
+            <button class='btn btn-outline-success menu-btn'> View Menu </button>
+        `;
+    }
+    $('#selection-container').html(selectionContainer_HTML)
 }
 
 
@@ -80,20 +67,20 @@ $(document).ready(function(){
     $('header').html(loadHeaderComponent('./'));
     $('footer').html(loadFooterComponent());
     $('#sideBar').html(loadSidebarComponent())
-    ordertypeManager('', true);
     $('#sideBar').hide();
-    
-    $('.order-typeSelector').click(function(){
-        ordertypeManager($(this).attr('id').split('-')[2]);
+    handleSelectionOptions('delivery')
+
+
+    $('#selection-manager').change(function(){
+        handleSelectionOptions($(this).val());
     })
 
-    $('#order-manageSection').on('click', '#order-findLocation', function(){ 
-        findUserCoordinates();
+    $('#selection-container').on('click','.menu-btn',function(){
+        window.location.href = './menu.html';
     });
 
-    $('*').on('click', '.menuPage-linker', function(){
-        window.location.href = './menu.html';
+    $('#locate-btn').click(function(){
+       findUserCoordinates();
     })
-
 })
 
